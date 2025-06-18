2f7c88d533cea855306b9b8fc8ad9c46c6294e63
JunEon <junun24@gmail.com>
2025-06-18 16:17:52 +0900

    enroll router

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from core.config import settings
+from auth.routes import router as auth_router
+from auth.routes import router as todos_router
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+
+api_prefix = settings.base_api_url
+
+
+app.include_router(auth_router, prefix=f"{api_prefix}/auth")
+app.include_router(todos_router, prefix=f"{api_prefix}/todos")
